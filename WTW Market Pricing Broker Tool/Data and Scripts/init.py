fc769013e2405355cb0972da7ca69ed8aa746c09
ACimini <alex.cimini@gmail.com>
2025-07-02 12:12:20 -0600

    Capstone File Update

--- WTW Market Pricing Broker Tool/Data and Scripts/init.py
+++
@@ -1,6 +0,0 @@
-from tower_parser import ClientProgram
-
-ClientProgram.all_to_csv('collapsed.csv', collapse_quota_shares=True)
-ClientProgram.all_to_csv('expanded.csv', collapse_quota_shares=False)
-ClientProgram.all_to_std_csv('collapsed_std.csv', collapse_quota_shares=True)
-ClientProgram.all_to_std_csv('expanded_std.csv', collapse_quota_shares=False)
